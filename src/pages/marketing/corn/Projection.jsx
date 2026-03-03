549b7670e34374d3c275a5167e97bdafd4c82c80
chopper123456 <cademcneil99@gmail.com>
2026-03-02 19:05:20 -0700

    fix corn projection wrapper

---
+++ src/pages/marketing/corn/Projection.jsx
@@ -0,0 +1,12 @@
+import CornProjectionTool from './CornProjectionTool'
+
+export default function CornProjection() {
+  return <CornProjectionTool />
+}
+```
+
+Save it, then run:
+```
+git add .
+git commit -m "fix corn projection wrapper"
+git push
